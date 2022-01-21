2b69e1b9682039b010e6ecd0728589d57050e36e
Sébastien Vanstavel <sebastien@retailshake.com>
2022-01-21 15:53:10 +0100

    Rollback node report page (#24)
    * H4r1c0t/alert threshold (#9)
    
    * feat: create function to manage number of items scraped and get vizual alert
    
    also modify the monitoring graph
    
    * Add some tests
    
    * :ambulance: (node_reports.py) hotfix on node_reports page
    
    try to momentarily desactivate the alert_threshold feature to fix the 500 error on scrapydweb node reports page
    
    Closes #1279
    
    * :ambulance: (node_reports.py) add try except
    
    add try/except on node reports page
    
    * :ambulance: (maths.py) fix DataFrame type
    
    * :bug: (node report) fix logger Error
    
    * fix(node-report-page): rollback node report page
    
    duplicate node reports page with two differents routes: \n- one for the original node reports page \n- the other for RS node reports page (nodereport_rs)
    
    Co-authored-by: Shake A Punk <86238654+guishake@users.noreply.github.com>

--- scrapydweb/views/dashboard/node_reports.py
+++ scrapydweb/views/dashboard/node_reports.py
@@ -1,12 +1,10 @@
 # coding: utf-8
 import json
-import pandas as pd
 
-from flask import render_template, url_for, current_app
+from flask import render_template, url_for
 
 from ..baseview import BaseView
-from ...utils.monitoring_tools import dataframes as mtd, maths as mtm, graphs as mtg
-from ...vars import DATABASE_URL
+
 
 class NodeReportsView(BaseView):
 
@@ -33,52 +31,10 @@ class NodeReportsView(BaseView):
             if not job['start']:
                 self.pending_jobs.append(job)
             else:
-                if job["finish"]:
-                    try: 
-                        spider_filter = f"spider = '{job['spider']}'"
-                        table_name = self.SCRAPYD_SERVER.replace(".", "_").replace(":", "_")
-                        con, db_type = mtd.db_connect(DATABASE_URL, return_db_type=True)
-                        if db_type == "sqlite":
-                            query = f"""
-                                SELECT * 
-                                FROM '{table_name}'
-                                WHERE {spider_filter}
-                                """
-                            df = mtd.jobs_df_format(pd.read_sql(query, con=con))
-                        elif db_type == "mysql":
-                            query = f"""
-                            SELECT *
-                            FROM {table_name}
-                            WHERE {spider_filter};
-                            """
-                            df = mtd.jobs_df_format(pd.read_sql(query, con=con))
-                        else:
-                            self.logger("Database type not handled yet...")
-                            return
+                if job['finish']:
+                    self.finished_jobs.append(job)
 
-                        df = mtd.jobs_df_format(pd.read_sql(query, con=con))
-                        
-                        # 
-                        df = mtd.select_last_date(df, "start_date")
-
-                        # Compute means
-                        df = mtm.compute_floating_means(df, "items", 7)  # Compute floating mean for items
-                        df = mtm.compute_floating_means(df, "pages", 7)  # Compute floating mean for pages
-                        
-                        # Compute standard deviations
-                        df = mtm.compute_floating_deviation(df, "items", 7)
-                        df = mtm.compute_floating_deviation(df, "pages", 7)
-
-                        # Set alert lvl 
-                        items_alert_lvl = mtm.set_alert_level(df, 'items')      # issue #1279 → variable 'scrap_result' referenced before assignment
-                        job['alert_indicator'] = mtm.check_alert_level(items_alert_lvl)
-
-                        self.finished_jobs.append(job)        
-                        
-                    except Exception as e:
-                        self.logger.error(f"Failed to get data from database:\n{e}")
                 else:
-                    job['alert_indicator'] = '🔄'
                     self.running_jobs.append(job)
 
         if self.JOBS_FINISHED_JOBS_LIMIT > 0:
